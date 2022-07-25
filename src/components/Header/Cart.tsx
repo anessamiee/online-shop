069c36c9c5437c54060e917b011a8914a7490c33
anessamiee <anessamiee@gmail.com>
2022-07-25 19:50:12 +0430

    header cart component created

---
+++ src/components/Header/Cart.tsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react'
+import CartContext from '../../store/cart-context'
+
+const Cart = () => {
+  const cartCtx = useContext(CartContext)
+  const length = cartCtx?.cartItems.length
+  console.log('Cart render')
+  return (
+    <div className='flex items-center justify-center '>
+      <div>Cart</div>
+      {!!length && (
+        <span className='ml-1 text-sm p-1 w-6 h-6 rounded-full bg-gray-900 text-white flex justify-center items-center'>
+          {length}
+        </span>
+      )}
+    </div>
+  )
+}
+export default React.memo(Cart)
