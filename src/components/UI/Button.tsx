6439b7d5e7a4b32b6e0442de6d85cda1f6552779
anessamiee <anessamiee@gmail.com>
2022-07-26 22:38:33 +0430

    button component created

---
+++ src/components/UI/Button.tsx
@@ -0,0 +1,21 @@
+type Props = {
+  className?: string
+  children: string
+  onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined
+  type?: 'button' | 'reset' | 'submit'
+}
+const Button: React.FC<Props> = ({ className, children, onClick, type }) => {
+  return (
+    <button
+      className={
+        'rounded-md bg-gray-500 text-gray-100 px-4 py-2 hover:bg-gray-400 hover:text-gray-900 hover:drop-shadow-xl transition-all ' +
+        className
+      }
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
+  )
+}
+export default Button
