b0dab04714b8715bbe4eb554f2ad2d1add64d53b
anessamiee <anessamiee@gmail.com>
2022-07-26 22:33:11 +0430

    routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
 import Layout from './components/UI/Layout'
+import Products from './pages/Products'
 
 const App: React.FC = () => {
-  return <Layout>asd</Layout>
+  return (
+    <Layout>
+      <Routes>
+        <Route path='/' element={<Products />} />
+      </Routes>
+    </Layout>
+  )
 }
 
 export default App
