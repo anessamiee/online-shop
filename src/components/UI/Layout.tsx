a6d0105b2c3b8b5f978dbf95e3f4893afe09d80f
anessamiee <anessamiee@gmail.com>
2022-07-26 22:38:55 +0430

    layout style improved

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -4,7 +4,7 @@ const Layout = (props: PropsWithChildren) => {
   return (
     <>
       <Header />
-      <main className='text-gray-900'>{props.children}</main>
+      <main className='text-gray-900 sm:px-16 px-8 py-4 antialiased selection:bg-gray-300'>{props.children}</main>
     </>
   )
 }
