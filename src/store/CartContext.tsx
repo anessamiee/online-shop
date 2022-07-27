c1e879b18a54f31dc71a4867c3cb7c8d59bb9be1
anessamiee <anessamiee@gmail.com>
2022-07-27 12:03:14 +0430

    initial values added, optimization in progress...

--- src/store/CartContext.tsx
+++ src/store/CartContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, PropsWithChildren, ReactNode, useState } from 'react'
+import React, { createContext, PropsWithChildren, ReactNode, useCallback, useState } from 'react'
 import { Product } from '../types/Product'
 
 type CartContext = {
@@ -6,7 +6,12 @@ type CartContext = {
   addToCart: (product: Product) => void
   removeFromCart: (id: number) => void
 }
-const CartContext = createContext<CartContext | null>(null)
+const initialValues: CartContext = {
+  cartItems: [],
+  addToCart: () => {},
+  removeFromCart: () => {},
+}
+const CartContext = createContext<CartContext>(initialValues)
 const dummyItem = {
   id: 1,
   title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
@@ -19,16 +24,18 @@ const dummyItem = {
 }
 export const CartContextProvider = (props: PropsWithChildren) => {
   const [cartItems, setCartItems] = useState<Product[]>([dummyItem])
-  const addToCart = (product: Product) => {
+  const addToCart = useCallback((product: Product) => {
     setCartItems((state) => [...state, product])
-  }
+  }, [])
   const removeFromCart = (id: number) => {
     setCartItems(cartItems.filter((item) => item.id !== id))
   }
-  const value = React.useMemo(() => {
-    return { cartItems, addToCart, removeFromCart }
-  }, [cartItems, addToCart, removeFromCart])
-  return <CartContext.Provider value={value}>{props.children}</CartContext.Provider>
+  console.log('cart context render')
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+      {props.children}
+    </CartContext.Provider>
+  )
 }
 
 export default CartContext
