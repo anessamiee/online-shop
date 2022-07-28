f6867390ef54e5ec9c6ac5081e677e59e54e62c6
anessamiee <anessamiee@gmail.com>
2022-07-28 16:13:01 +0430

    styles improved

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -4,7 +4,7 @@ import Search from '../components/Product/Search'
 const Products = () => {
   console.log('products page')
   return (
-    <div className='flex flex-col items-center justify-center pt-8 '>
+    <div className='flex flex-col items-center justify-center w-full'>
       <Search />
       <ProductsList />
     </div>
