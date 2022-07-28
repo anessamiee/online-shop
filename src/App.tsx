536cc64f871eb0b724bc998d1f1c10494a07efa1
anessamiee <anessamiee@gmail.com>
2022-07-28 16:08:44 +0430

    shopping page route added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/UI/Layout'
 import Products from './pages/Products'
+import ShoppingCart from './pages/ShoppingCart'
 
 const App: React.FC = () => {
   return (
     <Layout>
       <Routes>
         <Route path='/' element={<Products />} />
+        <Route path='/shopping-cart' element={<ShoppingCart />} />
       </Routes>
     </Layout>
   )
