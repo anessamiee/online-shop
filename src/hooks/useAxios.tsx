2266bcb39dc27520e0aaedf56d65f9e2b19c71a4
anessamiee <anessamiee@gmail.com>
2022-07-31 03:12:45 +0430

    bugs fixed

--- src/hooks/useAxios.tsx
+++ src/hooks/useAxios.tsx
@@ -1,17 +1,17 @@
 import { useState, useEffect } from 'react'
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
+import { AxiosResults } from '../types/AxiosResults'
 
 axios.defaults.baseURL = 'https://fakestoreapi.com'
 
-function useAxios<T>(axiosParams: AxiosRequestConfig) {
+function useAxios<T>(axiosParams: AxiosRequestConfig): AxiosResults<T> {
   const [response, setResponse] = useState<AxiosResponse<T>>()
   const [error, setError] = useState<AxiosError>()
   const [loading, setLoading] = useState(true)
-
   const fetchData = async (params: AxiosRequestConfig) => {
     try {
       const result = await axios.request(params)
-      if (result.data === null || result.data==='') {
+      if (result.data === null || result.data === '') {
         new AxiosError()
         const error = new AxiosError()
         error.code = 'ERR_BAD_REQUEST'
@@ -30,8 +30,8 @@ function useAxios<T>(axiosParams: AxiosRequestConfig) {
 
   useEffect(() => {
     fetchData(axiosParams)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-
   return { response, error, loading }
 }
 
