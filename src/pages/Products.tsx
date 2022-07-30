a2cbaac6dbfb24c68307bcdcc3430d9248ac7f4e
anessamiee <anessamiee@gmail.com>
2022-07-31 03:14:21 +0430

    .

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,8 +1,7 @@
 import ProductsList from '../components/Product/ProductsList'
 import Search from '../components/Product/Search'
 
-const Products = () => {
-  console.log('products page')
+const Products: React.FC = () => {
   return (
     <div className='flex flex-col items-center justify-center w-full'>
       <Search />
