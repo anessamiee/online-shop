cbd626723ae7623459aef5d755e1df2ae91ebc34
anessamiee <anessamiee@gmail.com>
2022-08-03 13:24:30 +0430

    in add action if item exist it will increase its quantity by 1

--- src/store/reducers/CartReducer.ts
+++ src/store/reducers/CartReducer.ts
@@ -28,6 +28,7 @@ export type CartProduct = {
 export type CartState = {
   products: CartProduct[]
   cartTotalPrice: number
+  allProductsNo: number
 }
 
 export type CartAction = ActionMap<CartPayload>[keyof ActionMap<CartPayload>]
@@ -39,9 +40,32 @@ export const cartReducer: Reducer<CartState, CartAction> = (state, action) => {
         const product = newCartProduct(newProduct)
         const products = [...state.products, product]
         const cartTotalPrice = getCartTotalPrice(products)
+        const allProductsNo = getAllProductsNo(products)
         return {
           products,
           cartTotalPrice,
+          allProductsNo,
+        }
+      } else if (isItemExist(newProduct.id, state.products)) {
+        const products = [
+          ...state.products.map((cart) => {
+            if (cart.product.id === newProduct.id) {
+              const quantity = cart.quantity + 1
+              return {
+                ...cart,
+                quantity,
+                productTotalPrice: setProductTotalPrice(cart.product.price, quantity),
+              }
+            }
+            return cart
+          }),
+        ]
+        const cartTotalPrice = getCartTotalPrice(products)
+        const allProductsNo = getAllProductsNo(products)
+        return {
+          products,
+          cartTotalPrice,
+          allProductsNo,
         }
       }
       return state
@@ -49,9 +73,11 @@ export const cartReducer: Reducer<CartState, CartAction> = (state, action) => {
     case ActionTypes.Remove: {
       const products = [...state.products.filter((state) => state.product.id !== action.payload.id)]
       const cartTotalPrice = getCartTotalPrice(products)
+      const allProductsNo = getAllProductsNo(products)
       return {
         products,
         cartTotalPrice,
+        allProductsNo,
       }
     }
     case ActionTypes.Update: {
@@ -71,9 +97,11 @@ export const cartReducer: Reducer<CartState, CartAction> = (state, action) => {
           }),
         ]
         const cartTotalPrice = getCartTotalPrice(products)
+        const allProductsNo = getAllProductsNo(products)
         return {
           products,
           cartTotalPrice,
+          allProductsNo,
         }
       }
       return state
@@ -82,7 +110,7 @@ export const cartReducer: Reducer<CartState, CartAction> = (state, action) => {
       return state
   }
 }
-const isItemExist = (id: number, state: CartProduct[]): boolean => {
+export const isItemExist = (id: number, state: CartProduct[]): boolean => {
   const newQuantityItem = state.find((item) => item.product.id === id)
   if (newQuantityItem === undefined) return false
   return true
@@ -102,4 +130,9 @@ const newCartProduct = (product: Product): CartProduct => {
   const quantity = 1
   const productTotalPrice = product.price
   return { product, quantity, productTotalPrice }
+}
+const getAllProductsNo = (state: CartProduct[]) => {
+  let total = 0
+  state.map((item) => (total += item.quantity))
+  return total
 }
